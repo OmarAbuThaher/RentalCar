e26c69b31e66058b1a9143c733f03989dd234c86
Omar Mohammed <o.abuthaher@gmail.com>
2023-08-23 19:57:41 +0300

    first commit

---
+++ resources/views/cms/template.blade.php
@@ -0,0 +1,19 @@
+@extends('cms.parent')
+@section('title','Temp') 
+@section('main-title','Main Temp') 
+@section('styles') 
+@endsection
+
+@section('content')
+
+    
+@endsection
+
+
+
+
+
+
+@section('scripts')
+    
+@endsection
